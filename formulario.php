e12fb112853b6b221c23d564621290a04fcbec2f
AntonioMartinezR <68454360+IngAntonioMtz@users.noreply.github.com>
2022-06-18 23:21:01 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Talabarteria La Dueña </title>
+
+	<meta charset="utf-8">
+	<meta keywords="">
+	<meta description="">
+	<meta name="viewport" content="width=device-width", user-scalable="no">
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" type="text/css" href="estilos/bootstrapNaranja.min.css">
+
+	<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
+	<link rel="stylesheet" type="text/css" href="estilos/estilosslide.css">
+	<!--link rel="stylesheet" type="text/css" href="estilos/estilosfooter.css"-->
+	<!--link rel="stylesheet" type="text/css" href="estilos/bootstrapNaranja.min"-->
+
+</head>
+	<header>
+		<title>Formulario</title>
+
+	</header>
+	<body>
+		<h1>Formulario para postularse para trabajar  </h1>
+		<form action='' method='post'>
+			<table>
+				<tr>
+					<th>
+						Nombre :
+					</th>	
+					<td>
+						<input type='text' name 'nombre'>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Apellido :
+					</th>	
+					<td>
+						<input type='text' name 'apellido'>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Edad :
+					</th>	
+					<td>
+						<input type='text' name 'edad'>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Sexo :
+					</th>	
+					<td>
+						<input type='text' name 'sexo'>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Correo Electronico :
+					</th>	
+					<td>
+						<input type='text' name 'correo'>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Numero telefonico :
+					</th>	
+					<td>
+						<input type='text' name 'celular'>
+					</td>
+				</tr>
+				
+
+			</table>
+			<input type='submit'> <input type ='reset'> 
+
+		</form>
+	</body>
+</html>			
